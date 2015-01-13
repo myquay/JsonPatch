60be3ce59a598ee8562b7348daf6ec3d07750831
Wade Gausden <wade.gausden@gmail.com>
2015-01-13 14:08:13 +1300

    Diff Helper can generate diffs of simple objects.

---
+++ src/JsonPatch.Tests/DiffHelperTests.cs
@@ -0,0 +1,43 @@
+﻿using JsonPatch.Helpers;
+using JsonPatch.Tests.Entitys;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonPatch.Tests
+{
+    [TestClass]
+    public class DiffHelperTests
+    {
+        [TestMethod]
+        public void GenerateDiff_SimpleEntityReplace_ReturnsReplaceOperation()
+        {
+            SimpleEntity originalDocument = new SimpleEntity() { Foo = "bar" };
+            SimpleEntity modifiedDocument = new SimpleEntity() { Foo = "baz" };
+
+            List<JsonPatchOperation> operations = DiffHelper.GenerateDiff(typeof(SimpleEntity), originalDocument, modifiedDocument).ToList();
+
+            Assert.AreEqual(1, operations.Count);
+            Assert.AreEqual(JsonPatchOperationType.replace, operations.First().Operation);
+            Assert.AreEqual("/Foo", operations.First().PropertyName);
+            Assert.AreEqual(modifiedDocument.Foo, operations.First().Value);
+        }
+
+        [TestMethod]
+        public void GenerateDiff_SimpleEntityRemove_ReturnsRemoveOperation()
+        {
+            SimpleEntity originalDocument = new SimpleEntity() { Foo = "bar" };
+            SimpleEntity modifiedDocument = new SimpleEntity() { Foo = null };
+
+            List<JsonPatchOperation> operations = DiffHelper.GenerateDiff(typeof(SimpleEntity), originalDocument, modifiedDocument).ToList();
+
+            Assert.AreEqual(1, operations.Count);
+            Assert.AreEqual(JsonPatchOperationType.remove, operations.First().Operation);
+            Assert.AreEqual("/Foo", operations.First().PropertyName);
+            Assert.AreEqual(modifiedDocument.Foo, operations.First().Value);
+        }
+    }
+}
---
+++ src/JsonPatch/Helpers/DiffHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonPatch.Helpers
+{
+    public static class DiffHelper
+    {
+        public static IEnumerable<JsonPatchOperation> GenerateDiff(Type entity, object originalDocument, object modifiedDocument, string path = "/")
+        {
+            if (originalDocument.GetType() != entity)
+                throw new ArgumentException(string.Format("Original Document is type of {0} but requested type is {1}", originalDocument.GetType().ToString(), entity.ToString()));
+
+            if (modifiedDocument.GetType() != entity)
+                throw new ArgumentException(string.Format("Modified Document is type of {0} but requested type is {1}", modifiedDocument.GetType().ToString(), entity.ToString()));
+
+            var propertyList = entity.GetProperties();
+            foreach (var property in propertyList)
+            {
+                var originalValue = property.GetValue(originalDocument);
+                var modifiedValue = property.GetValue(modifiedDocument);
+
+                if (originalValue != modifiedValue)
+                {
+                    yield return new JsonPatchOperation()
+                    {
+                        Operation = modifiedValue == null ? JsonPatchOperationType.remove : JsonPatchOperationType.replace,
+                        PropertyName = path + property.Name,
+                        Value = property.GetValue(modifiedDocument)
+                    };
+                }
+            }
+
+            yield break;
+        }
+    }
+}
