ada2a40d0fb9cccf26c2e2ad9a2c7585b3d9fb64
Broderick T <broderickt@rev.io>
2023-11-19 16:21:14 -0500

    Remove unused using statements

--- src/JsonPatch/JsonPatchDocument.cs
+++ src/JsonPatch/JsonPatchDocument.cs
@@ -1,13 +1,8 @@
-﻿using System;
+﻿using JsonPatch.Formatting;
+using JsonPatch.Paths.Resolvers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using JsonPatch;
-using JsonPatch.Paths;
-using JsonPatch.Paths.Resolvers;
-using JsonPatch.Formatting;
 
 namespace JsonPatch
 {
